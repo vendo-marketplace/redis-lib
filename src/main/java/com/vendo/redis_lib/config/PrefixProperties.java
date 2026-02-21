1d6c81cab8c81b2e4f0b1087064306151431f7d8
vlcdck <pridatchenkovladislav@gmail.com>
2026-02-21 18:00:04 +0200

    removing lombok

--- src/main/java/com/vendo/redis_lib/config/PrefixProperties.java
+++ src/main/java/com/vendo/redis_lib/config/PrefixProperties.java
@@ -1,16 +1,55 @@
 package com.vendo.redis_lib.config;
 
-import lombok.Data;
+import java.util.Objects;
 
-@Data
 public class PrefixProperties {
 
     private String prefix;
 
     private long ttl;
 
+    public PrefixProperties(String prefix) {
+    }
+
+    public PrefixProperties(String prefix, long ttl) {
+        this.prefix = prefix;
+        this.ttl = ttl;
+    }
+
     public String buildPrefix(String value) {
         return prefix + value;
     }
 
+    public long getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(long ttl) {
+        this.ttl = ttl;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PrefixProperties that = (PrefixProperties) o;
+        return ttl == that.ttl && Objects.equals(prefix, that.prefix);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(prefix, ttl);
+    }
+
+    @Override
+    public String toString() {
+        return "PrefixProperties{" +
+                "prefix='" + prefix + '\'' +
+                ", ttl=" + ttl +
+                '}';
+    }
 }
